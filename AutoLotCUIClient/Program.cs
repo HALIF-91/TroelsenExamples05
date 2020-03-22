aca95fcd3399cf5d8e198f9cd5a1ef35e20b3737
ADIL <globalmail91@gmail.com>
2020-03-22 18:50:14 +0600

    Работа с БД

---
+++ AutoLotCUIClient/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using AutoLotConnectedLayer;
+using System.Configuration;
+using System.Data;
+
+namespace AutoLotCUIClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** The AutoLot Console UI *******\n");
+
+            // Получить строку соединения из App.config
+            string cnStr = ConfigurationManager.ConnectionStrings["AutoLotSqlProvider"].ConnectionString;
+            bool userDone = false;
+            string userCommand = "";
+
+            // Создать объект InventoryDAL
+            InventoryDAL invDAL = new InventoryDAL();
+            invDAL.OpenConnection(cnStr);
+
+            // Продолжать запрашивать у пользователя ввод вплоть до получения команды Q
+
+            try
+            {
+                ShowInstructions();
+                do
+                {
+                    Console.Write("\nPlease enter your command: ");
+                    userCommand = Console.ReadLine();
+                    Console.WriteLine();
+                    switch (userCommand.ToUpper())
+                    {
+                        case "I":
+                            InsertNewCar(invDAL); break;
+                        case "U":
+                            UpdateCarPetName(invDAL); break;
+                        case "D":
+                            DeleteCar(invDAL); break;
+                        case "L":
+                            ListInventory(invDAL); break;
+                        case "S":
+                            ShowInstructions(); break;
+                        case "P":
+                            LookUpPetName(invDAL); break;
+                        case "Q":
+                            userDone = true; break;
+                        default:
+                            Console.WriteLine("Bad data! Try again"); break;
+                    }
+                } while (!userDone);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                invDAL.CloseConnection();
+            }
+        }
+
+        private static void LookUpPetName(InventoryDAL invDAL)
+        {
+            // Получить идентификатор автомобиля для поиска дружественного имени
+            Console.Write("Enter ID of car to look up: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("PetName of {0} is {1}", id, invDAL.LookUpPetName(id).TrimEnd());
+        }
+
+        private static void UpdateCarPetName(InventoryDAL invDAL)
+        {
+            // Сначала получить пользовательские данные
+            int CarID;
+            string newCarPetName;
+
+            Console.Write("Enter Car ID: ");
+            CarID = int.Parse(Console.ReadLine());
+            Console.Write("Enter new Pet name: ");
+            newCarPetName = Console.ReadLine();
+
+            // Теперь передать информацию библиотеке доступа к данным
+            invDAL.UpdateCarPetName(CarID, newCarPetName);
+        }
+
+        private static void InsertNewCar(InventoryDAL invDAL)
+        {
+            // Сначала получить пользовательские данные
+            int newCarID;
+            string newCarColor, newCarMake, newCarPetName;
+            Console.Write("Enter Car ID: ");
+            newCarID = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter Car color: ");
+            newCarColor = Console.ReadLine();
+
+            Console.Write("Enter Car make: ");
+            newCarMake = Console.ReadLine();
+
+            Console.Write("Enter Pet name: ");
+            newCarPetName = Console.ReadLine();
+
+            // Теперь передать информацию библиотеке доступа к данным
+            NewCar car = new NewCar
+            {
+                CarID = newCarID,
+                Color = newCarColor,
+                Make = newCarMake,
+                PetName = newCarPetName
+            };
+
+            invDAL.InsertAuto(car);
+        }
+
+        private static void DeleteCar(InventoryDAL invDAL)
+        {
+            // Получить идентификатор удаляемого автомобиля
+            Console.Write("Enter ID of Car to delete: ");
+            int id = int.Parse(Console.ReadLine());
+
+            // На случай нарушения ссылочной целостности
+            try
+            {
+                invDAL.DeleteCar(id);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void ShowInstructions()
+        {
+            Console.WriteLine("I: inserts a new car"); // вставить новый автомобиль
+            Console.WriteLine("U: updates an existing car"); // обновить сущ. автомобиль
+            Console.WriteLine("D: deletes an existing car"); // удалить сущ. автомобиль
+            Console.WriteLine("L: lists current inventory"); // вывести текущие запасы
+            Console.WriteLine("S: shows these instructions"); // вывести эти инструкции
+            Console.WriteLine("P: looks up pet name"); // найти дружественное имя автомобиля
+            Console.WriteLine("Q: quits program"); // завершить программу
+        }
+
+        static void ListInventory(InventoryDAL invDAL)
+        {
+            // получить список автомобилей на складе
+            DataTable dt = invDAL.GetAllInventoryAsDataTable();
+            // передать DataTable вспомогательной функции для отображения
+            DisplayTable(dt);
+        }
+
+        static void DisplayTable(DataTable dt)
+        {
+            // Вывести имена столбцов
+            for (int curCol = 0; curCol < dt.Columns.Count; curCol++)
+            {
+                Console.Write(dt.Columns[curCol].ColumnName + "\t");
+            }
+            Console.WriteLine("\n-----------------------------------");
+
+            // вывести содердимое DataTable
+            for (int curRow = 0; curRow < dt.Rows.Count; curRow++)
+            {
+                for (int curCol = 0; curCol < dt.Columns.Count; curCol++)
+                {
+                    Console.Write(dt.Rows[curRow][curCol].ToString() + "\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ AutoLotDAL/AutoLotConnDAL.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AutoLotConnectedLayer
+{
+    public class InventoryDAL
+    {
+        // Этот член будет использоваться всеми методами
+        private SqlConnection sqlCn = null;
+        public void OpenConnection(string connectionString)
+        {
+            sqlCn = new SqlConnection();
+            sqlCn.ConnectionString = connectionString;
+            sqlCn.Open();
+        }
+        public void CloseConnection()
+        {
+            sqlCn.Close();
+        }
+        public void InsertAuto(NewCar car)
+        {
+            // Сформировать SQL-оператор
+            string sql = string.Format("Insert into Inventory" +
+                "(CarID, Make, Color, PetName) Values" +
+                "('{0}', '{1}', '{2}', '{3}')", car.CarID, car.Make, car.Color, car.PetName);
+
+            // Выполнить SQL оператор с применением нашего подключения
+            using (SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void InsertAuto(int id, string color, string make, string petName)
+        {
+            // Обратите внимание на "заполнители" в SQL запросе
+            string sql = string.Format("Insert into Inventory " +
+                "(CarID, Make, Color, PetName) values " +
+                "(@CarID, @Make, @Color, @PetName)");
+
+            // Эта команда будет иметь внутренние параметры
+            using (SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                // Заполнить коллекцию параметров
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@CarID";
+                param.Value = id;
+                param.SqlDbType = SqlDbType.Int;
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter();
+                param.ParameterName = "@Make";
+                param.Value = make;
+                param.SqlDbType = SqlDbType.Char;
+                param.Size = 10;
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter();
+                param.ParameterName = "@Color";
+                param.Value = color;
+                param.SqlDbType = SqlDbType.Char;
+                param.Size = 10;
+                cmd.Parameters.Add(param);
+
+                param = new SqlParameter();
+                param.ParameterName = "@PetName";
+                param.Value = petName;
+                param.SqlDbType = SqlDbType.Char;
+                param.Size = 10;
+                cmd.Parameters.Add(param);
+
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public void DeleteCar(int id)
+        {
+            // Получить идентификатор удаляемого автомобиля, затем выполнить удаление
+            string sql = string.Format("Delete from Inventory where CarID = '{0}'", id);
+
+            using (SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException ex)
+                {
+                    Exception error = new Exception("Sorry! That car is on order!", ex);
+                    throw error;
+                }
+            }
+        }
+        public void UpdateCarPetName(int id, string newPetName)
+        {
+            // Получить идентификатор модифицируемого автомобиля и новое дружественное имя
+            string sql = string.Format("Update Inventory Set PetName = '{0}' Where CarID = '{1}'", newPetName, id);
+
+            using (SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
+        public List<NewCar> GetAllInventoryAsList()
+        {
+            // Здесь будут храниться записи
+            List<NewCar> inv = new List<NewCar>();
+
+            // Подготовить объект команды
+            string sql = "Select * from Inventory";
+
+            using(SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    inv.Add(new NewCar
+                    {
+                        CarID = (int)dr["CarID"],
+                        Color = (string)dr["Color"],
+                        Make = (string)dr["Make"],
+                        PetName = (string)dr["PetName"]
+                    });
+                }
+                dr.Close();
+            }
+            return inv;
+        }
+        public DataTable GetAllInventoryAsDataTable()
+        {
+            // Здесь будут храниться записи
+            DataTable inv = new DataTable();
+
+            // Подготовить объект команды
+            string sql = "Select * from Inventory";
+            using(SqlCommand cmd = new SqlCommand(sql, this.sqlCn))
+            {
+                SqlDataReader dr = cmd.ExecuteReader();
+
+                // Заполнить DataTable данными из объекта чтения и выполнить очистку
+                inv.Load(dr);
+                dr.Close();
+            }
+            return inv;
+        }
+        public string LookUpPetName(int carID)
+        {
+            string carPetName = string.Empty;
+
+            // Установить имя хранимой процедуры
+            using (SqlCommand cmd = new SqlCommand("GetPetName", this.sqlCn))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                // Входной параметр
+                SqlParameter param = new SqlParameter();
+                param.ParameterName = "@carID";
+                param.SqlDbType = SqlDbType.Int;
+                param.Value = carID;
+
+                // По умолчанию параметры считаются входными (Input), но все же для ясности
+                param.Direction = ParameterDirection.Input;
+                cmd.Parameters.Add(param);
+
+                // Выходной паратметр
+                param = new SqlParameter();
+                param.ParameterName = "@petName";
+                param.SqlDbType = SqlDbType.Char;
+                param.Size = 10;
+                param.Direction = ParameterDirection.Output;
+                cmd.Parameters.Add(param);
+
+                // Выполнить хранимую процедуру
+                cmd.ExecuteNonQuery();
+
+                // Возвратить выходной параметр
+                carPetName = (string)cmd.Parameters["@petName"].Value;
+            }
+            return carPetName;
+        }
+    }
+}
