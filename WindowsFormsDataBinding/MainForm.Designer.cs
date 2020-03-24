952f68f09f00ef63d45c1a863bd3944787dd1df5
ADIL <globalmail91@gmail.com>
2020-03-24 20:18:52 +0600

    DataGridView

---
+++ WindowsFormsDataBinding/MainForm.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace WindowsFormsDataBinding
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.carInventoryGridView = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRowToRemove = new System.Windows.Forms.TextBox();
+            this.btnRemoveRow = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtMakeToView = new System.Windows.Forms.TextBox();
+            this.btnDisplayMakes = new System.Windows.Forms.Button();
+            this.btnChangeMakes = new System.Windows.Forms.Button();
+            this.dataGridYugosView = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.carInventoryGridView)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridYugosView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // carInventoryGridView
+            // 
+            this.carInventoryGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.carInventoryGridView.Location = new System.Drawing.Point(13, 69);
+            this.carInventoryGridView.Name = "carInventoryGridView";
+            this.carInventoryGridView.Size = new System.Drawing.Size(775, 261);
+            this.carInventoryGridView.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(300, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Here is what we have in stock";
+            // 
+            // txtRowToRemove
+            // 
+            this.txtRowToRemove.Location = new System.Drawing.Point(16, 44);
+            this.txtRowToRemove.Name = "txtRowToRemove";
+            this.txtRowToRemove.Size = new System.Drawing.Size(109, 20);
+            this.txtRowToRemove.TabIndex = 2;
+            // 
+            // btnRemoveRow
+            // 
+            this.btnRemoveRow.Location = new System.Drawing.Point(163, 42);
+            this.btnRemoveRow.Name = "btnRemoveRow";
+            this.btnRemoveRow.Size = new System.Drawing.Size(83, 23);
+            this.btnRemoveRow.TabIndex = 3;
+            this.btnRemoveRow.Text = "Delete";
+            this.btnRemoveRow.UseVisualStyleBackColor = true;
+            this.btnRemoveRow.Click += new System.EventHandler(this.btnRemoveRow_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnRemoveRow);
+            this.groupBox1.Controls.Add(this.txtRowToRemove);
+            this.groupBox1.Location = new System.Drawing.Point(13, 347);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(371, 91);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Enter ID of Car to Delete";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnDisplayMakes);
+            this.groupBox2.Controls.Add(this.txtMakeToView);
+            this.groupBox2.Location = new System.Drawing.Point(410, 347);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(378, 91);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Enter Make to View";
+            // 
+            // txtMakeToView
+            // 
+            this.txtMakeToView.Location = new System.Drawing.Point(26, 44);
+            this.txtMakeToView.Name = "txtMakeToView";
+            this.txtMakeToView.Size = new System.Drawing.Size(100, 20);
+            this.txtMakeToView.TabIndex = 0;
+            // 
+            // btnDisplayMakes
+            // 
+            this.btnDisplayMakes.Location = new System.Drawing.Point(163, 42);
+            this.btnDisplayMakes.Name = "btnDisplayMakes";
+            this.btnDisplayMakes.Size = new System.Drawing.Size(75, 23);
+            this.btnDisplayMakes.TabIndex = 1;
+            this.btnDisplayMakes.Text = "View";
+            this.btnDisplayMakes.UseVisualStyleBackColor = true;
+            this.btnDisplayMakes.Click += new System.EventHandler(this.btnDisplayMakes_Click);
+            // 
+            // btnChangeMakes
+            // 
+            this.btnChangeMakes.Location = new System.Drawing.Point(436, 26);
+            this.btnChangeMakes.Name = "btnChangeMakes";
+            this.btnChangeMakes.Size = new System.Drawing.Size(352, 23);
+            this.btnChangeMakes.TabIndex = 2;
+            this.btnChangeMakes.Text = "Change allBMWs to Yugos";
+            this.btnChangeMakes.UseVisualStyleBackColor = true;
+            this.btnChangeMakes.Click += new System.EventHandler(this.btnChangeMakes_Click);
+            // 
+            // dataGridYugosView
+            // 
+            this.dataGridYugosView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridYugosView.Location = new System.Drawing.Point(13, 489);
+            this.dataGridYugosView.Name = "dataGridYugosView";
+            this.dataGridYugosView.Size = new System.Drawing.Size(775, 179);
+            this.dataGridYugosView.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(12, 450);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(126, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Only Yugos";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 699);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridYugosView);
+            this.Controls.Add(this.btnChangeMakes);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.carInventoryGridView);
+            this.Name = "MainForm";
+            this.Text = "Windows Forms Data Binding";
+            ((System.ComponentModel.ISupportInitialize)(this.carInventoryGridView)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridYugosView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView carInventoryGridView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRowToRemove;
+        private System.Windows.Forms.Button btnRemoveRow;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnDisplayMakes;
+        private System.Windows.Forms.TextBox txtMakeToView;
+        private System.Windows.Forms.Button btnChangeMakes;
+        private System.Windows.Forms.DataGridView dataGridYugosView;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsDataBinding/MainForm.cs
@@ -0,0 +1,154 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
+using System;
+
+namespace WindowsFormsDataBinding
+{
+    public partial class MainForm : Form
+    {
+        // Коллекция объектов Car
+        List<Car> listCars = null;
+        // Складская информация
+        DataTable inventoryTable = new DataTable();
+        // Представление DataView
+        DataView yugosOnlyView;
+        public MainForm()
+        {
+            InitializeComponent();
+            // Заполнить список автомобилями
+            listCars = new List<Car>
+            {
+                new Car { ID = 100, PetName = "Chucky", Make = "BMW", Color = "Green" },
+                new Car { ID = 101, PetName = "Tiny", Make = "Yugo", Color = "White" },
+                new Car { ID = 102, PetName = "Ami", Make = "Jeep", Color = "Tan" },
+                new Car { ID = 103, PetName = "Pain Inducer", Make = "Caravan", Color = "Pink" },
+                new Car { ID = 104, PetName = "Fred", Make = "BMW", Color = "Green" },
+                new Car { ID = 105, PetName = "Sidd", Make = "BMW", Color = "Black" },
+                new Car { ID = 106, PetName = "Mel", Make = "Firebird", Color = "Red" },
+                new Car { ID = 107, PetName = "Sarah", Make = "Colt", Color = "Black" }
+            };
+
+            // Создать таблицу данных
+            CreateDataTable();
+            //ShowCarsWithIdGreaterThan();
+
+            // Создать представление
+            CreateDataView();
+        }
+        private void CreateDataTable()
+        {
+            // Создать схему таблицы
+            DataColumn carIDColumn = new DataColumn("ID", typeof(int));
+            DataColumn carMakeColumn = new DataColumn("Make", typeof(string));
+            DataColumn carColorColumn = new DataColumn("Color", typeof(string));
+            DataColumn carPetNameColumn = new DataColumn("PetName", typeof(string));
+            carPetNameColumn.Caption = "Pet Name";
+            inventoryTable.Columns.AddRange(new DataColumn[]
+            {
+                carIDColumn, carMakeColumn, carColorColumn, carPetNameColumn
+            });
+
+            // Пройти по List<T> для создания строк
+            foreach (Car c in listCars)
+            {
+                DataRow newRow = inventoryTable.NewRow();
+                newRow["ID"] = c.ID;
+                newRow["Make"] = c.Make;
+                newRow["Color"] = c.Color;
+                newRow["PetName"] = c.PetName;
+                inventoryTable.Rows.Add(newRow);
+            }
+
+            // Привязать DataTable к CarInventoryGridView
+            carInventoryGridView.DataSource = inventoryTable;
+        }
+        private void CreateDataView()
+        {
+            // Установить таблицу, которая используется для создания этого представления
+            yugosOnlyView = new DataView(inventoryTable);
+
+            // Сконфигурировать представление с помощью фильтра
+            yugosOnlyView.RowFilter = "Make='Yugo'";
+
+            // Привязать к новому элементу
+            dataGridYugosView.DataSource = yugosOnlyView;
+        }
+        // Удалить эту строку из DataRowCollection
+        private void btnRemoveRow_Click(object sender, System.EventArgs e)
+        {
+            try
+            {
+                // Найти правильную строку для удаления
+                DataRow[] rowToDelete = inventoryTable.Select(string.Format("ID={0}", int.Parse(txtRowToRemove.Text)));
+
+                // Удалить ее
+                rowToDelete[0].Delete();
+                inventoryTable.AcceptChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnDisplayMakes_Click(object sender, EventArgs e)
+        {
+            // Построить фильтр на основе пользовательского ввода
+            string filterStr = string.Format("Make='{0}'", txtMakeToView.Text);
+
+            // Найти все строки, удовлетворяющие фильтру
+            DataRow[] makes = inventoryTable.Select(filterStr, "PetName DESC");
+            //DataRow[] makes = inventoryTable.Select(filterStr);
+
+            if (makes.Length == 0)
+                MessageBox.Show("Sorry, no cars...", "Selection error!");
+            else
+            {
+                string strMake = "";
+                for (int i = 0; i < makes.Length; i++)
+                {
+                    // Получить значение PetName из текущей строки
+                    strMake += makes[i]["PetName"] + "\n";
+                }
+
+                // Вывести имена всех найденных автомобилей указанной марки
+                MessageBox.Show(strMake, string.Format("We have {0}s named:", txtMakeToView.Text));
+            }
+        }
+        private void ShowCarsWithIdGreaterThan()
+        {
+            // Вывести дружественные имена всех автомобилей со значением ID больше 105
+            DataRow[] properIDs;
+            string newFilterStr = "ID > 105";
+            properIDs = inventoryTable.Select(newFilterStr);
+            string strIDs = null;
+            for (int i = 0; i < properIDs.Length; i++)
+            {
+                DataRow temp = properIDs[i];
+                strIDs += temp["PetName"] + " is ID " + temp["ID"] + "\n";
+            }
+            MessageBox.Show(strIDs, "Pet names of cars where ID > 105");
+        }
+        // Найти с помощью фильтра все строки, которые нужно отредактировать
+        private void btnChangeMakes_Click(object sender, EventArgs e)
+        {
+            // Подтвердить выбор
+            if (DialogResult.Yes == MessageBox.Show("Are you sure??? BMWs are much nicer than Yugos!", "Please Confirm!", MessageBoxButtons.YesNo))
+            {
+                // Построить фильтр
+                string filterStr = "Make='BMW'";
+                string strMake = string.Empty;
+
+                // Найти все строки, удовлетворяющие фильтру
+                DataRow[] makes = inventoryTable.Select(filterStr);
+
+                // Заменить все BMW на Yugo
+                for (int i = 0; i < makes.Length; i++)
+                {
+                    makes[i]["Make"] = "Yugo";
+                }
+            }
+        }
+    }
+}
