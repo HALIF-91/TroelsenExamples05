7950dff9daf9fc69000a27db1a17020492ec74a7
ADIL <globalmail91@gmail.com>
2020-03-24 23:30:10 +0600

    SqlCommandBuilder

---
+++ InventoryDALDisconnectedGUI/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace InventoryDALDisconnectedGUI
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpdateInventory = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inventoryGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpdateInventory
+            // 
+            this.btnUpdateInventory.Location = new System.Drawing.Point(683, 415);
+            this.btnUpdateInventory.Name = "btnUpdateInventory";
+            this.btnUpdateInventory.Size = new System.Drawing.Size(93, 23);
+            this.btnUpdateInventory.TabIndex = 0;
+            this.btnUpdateInventory.Text = "Обновить БД";
+            this.btnUpdateInventory.UseVisualStyleBackColor = true;
+            this.btnUpdateInventory.Click += new System.EventHandler(this.btnUpdateInventory_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(7, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(568, 26);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Тестирование функциональности автономного уровня";
+            // 
+            // inventoryGrid
+            // 
+            this.inventoryGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inventoryGrid.Location = new System.Drawing.Point(12, 46);
+            this.inventoryGrid.Name = "inventoryGrid";
+            this.inventoryGrid.Size = new System.Drawing.Size(764, 351);
+            this.inventoryGrid.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.inventoryGrid);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnUpdateInventory);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.inventoryGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdateInventory;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView inventoryGrid;
+    }
+}
+
